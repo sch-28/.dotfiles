3046eafb995cced36999b98363b1c8dcc8c6d3b1
sch-28 <jansch2803@googlemail.com>
2025-10-12 21:17:10 +0200

    fix: polybar issue

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,12 +10,19 @@ killall -q polybar
 BAR_NAME=main
 BAR_CONFIG=/home/$USER/.config/polybar/config
 
+SCREENS=$(xrandr --query | grep " connected" | wc -l)
 PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
 OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
 
 # Launch on primary monitor
 MONITOR=$PRIMARY polybar --reload -c $BAR_CONFIG $BAR_NAME &
 sleep 1
+
+# If only one screen, quit
+if [ $SCREENS -eq 1 ]; then
+ exit 0
+fi
+
 
 # Launch on all other monitors
 for m in $OTHERS; do
