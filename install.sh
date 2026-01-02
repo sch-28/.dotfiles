08acce8e765110f7dbcb4d06019c0fba866571d9
sch-28 <jansch2803@googlemail.com>
2026-01-02 19:04:24 +0100

    feat: improvements

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ log "Running full update"
 bash ./update.sh
 
 log "Install required pacman packages"
-sudo pacman -S bob feh rustup zsh tmux zoxide kitty stow polybar rofi kmonad python-i3ipc docker lazydocker nvm pulsemixer dunst xclip flameshot jre-openjdk dbeaver maven clamav redshift
+sudo pacman -S bob feh rustup zsh tmux zoxide kitty stow polybar rofi kmonad python-i3ipc docker lazydocker nvm pulsemixer dunst xclip flameshot jre-openjdk dbeaver maven clamav redshift xorg-xsetroot
 
 log "Install required yay packages"
 yay -S greenclip
--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -25,25 +25,25 @@ blink.setup({
     sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
         providers = {
-            lsp = {
-                name = 'LSP',
-                module = 'blink.cmp.sources.lsp',
-                opts = {}, -- Passed to the source directly, varies by source
-                --- NOTE: All of these options may be functions to get dynamic behavior
-                --- See the type definitions for more information
-                enabled = true, -- Whether or not to enable the provider
-                async = true, -- Whether we should show the completions before this provider returns, without waiting for it
-                timeout_ms = 250, -- How long to wait for the provider to return before showing completions and treating it as asynchronous
-                transform_items = nil, -- Function to transform the items before they're returned
-                should_show_items = true, -- Whether or not to show the items
-                max_items = nil, -- Maximum number of items to display in the menu
-                min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
-                -- If this provider returns 0 items, it will fallback to these providers.
-                -- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback
-                fallbacks = {},
-                score_offset = 0, -- Boost/penalize the score of the items
-                override = nil, -- Override the source's functions
-            }
+            -- lsp = {
+            --     name = 'LSP',
+            --     module = 'blink.cmp.sources.lsp',
+            --     opts = {}, -- Passed to the source directly, varies by source
+            --     --- NOTE: All of these options may be functions to get dynamic behavior
+            --     --- See the type definitions for more information
+            --     enabled = true, -- Whether or not to enable the provider
+            --     async = true, -- Whether we should show the completions before this provider returns, without waiting for it
+            --     timeout_ms = 250, -- How long to wait for the provider to return before showing completions and treating it as asynchronous
+            --     transform_items = nil, -- Function to transform the items before they're returned
+            --     should_show_items = true, -- Whether or not to show the items
+            --     max_items = nil, -- Maximum number of items to display in the menu
+            --     min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
+            --     -- If this provider returns 0 items, it will fallback to these providers.
+            --     -- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback
+            --     fallbacks = {},
+            --     score_offset = 0, -- Boost/penalize the score of the items
+            --     override = nil, -- Override the source's functions
+            -- }
         },
     },
     completion = {
@@ -95,6 +95,8 @@ blink.setup({
             ['<Tab>'] = { 'select_next', 'fallback' },
             ['<S-Tab>'] = { 'select_prev', 'fallback' },
             ['<C-y>'] = { 'accept_and_enter', 'fallback' },
+            ['<C-n>'] = { 'select_next', 'fallback' },
+            ['<C-p>'] = { 'select_prev', 'fallback' },
         },
         completion = {
             menu = { auto_show = true },
